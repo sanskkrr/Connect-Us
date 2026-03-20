ab6bf4470aadd471df1d028dd25630c222b95934
Sanskkrr <SanskarChouksey2006@gmail.com>
2026-03-20 22:20:07 +0530

    Extended user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+from django.conf import settings
+class Profile ( models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
+    def __str__(self):
+        return self.user.username

7d89ce56ce747935456f6c87aa8440655e355a95
Sanskkrr <SanskarChouksey2006@gmail.com>
2026-04-01 12:11:44 +0530

    ready for render deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: daphne projectFolder.asgi:application --bind 0.0.0.0 --port $PORT
--- projectFolder/settings.py
+++ projectFolder/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-wq47(c9-!qwnbjad+yib95_ci5%qw6xf!nwlh67lm0k%kqmlt_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition

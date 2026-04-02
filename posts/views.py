8a305c8a46fcbc4dd29a8d95776d17ed9978db3d
Sanskkrr <SanskarChouksey2006@gmail.com>
2026-04-02 13:29:24 +0530

    fix wrong import request

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render,get_object_or_404,request
+from django.shortcuts import redirect, render,get_object_or_404
 
 from projectFolder import posts
 from .models import Post
